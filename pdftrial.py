c9d7529d2dce590a158d504cf3f41253208a7264
hpa528 <hp528@rutgers.edu>
2023-02-11 14:26:34 -0500

    Main

---
+++ pdftrial.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+pdffileobj = open('trial.pdf', 'rb')
+pdfreader = PyPDF2.PdfReader(pdffileobj)
+#x = len(pdfreader.pages)
+pageobj = pdfreader.pages[0]
+text = pageobj.extract_text()
+file1 = open("waiver.txt", 'w')
+file1.writelines(text)
