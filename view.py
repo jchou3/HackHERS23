dd69127f8a9a40413f3d28f5d1ddb5f2ead42271
hpa528 <hp528@rutgers.edu>
2023-02-12 02:15:36 -0500

    figuring out html upload

--- view.py
+++ view.py
@@ -7,6 +7,14 @@ from threading import Thread
 from time import sleep
 import speech_recognition as sr
 import pyttsx3
+import pdftotxt as ptt
+from bs4 import BeautifulSoup as s
+import requests
+import os
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = ''
 
 synthesizer = pyttsx3.init()
 
@@ -16,15 +24,18 @@ synthesizer.setProperty('rate', 175)
 app = Flask(__name__)
 r = sr.Recognizer()
 
-synthesizer.say("Hello, Welcome. Please hold the space bar and say “read” followed by the article’s name you want to summarize.") 
-synthesizer.runAndWait() 
-synthesizer.stop()
+#synthesizer.say("Hello, Welcome. Please hold the space bar and say “read” followed by the article’s name you want to summarize.") 
+#synthesizer.runAndWait() 
+#synthesizer.stop()
+# 
+#s = requests.get('http://127.0.0.1:8000').text
+#val = soup(s, 'html.parser').select_one('input#file')['value']
 
 def on_press(key):
 
     print("Key noticed")
     check_key(key)
-    while (True):
+    """   while (True):
         with sr.Microphone() as source:
         # read the audio data from the default microphone
         #print("Speak now, please.")
@@ -42,7 +53,7 @@ def on_press(key):
                     decipher(text)
                     return
             except:
-                print("An exception occurred")
+                print("An exception occurred") """
                 
 
     
@@ -74,8 +85,22 @@ def keyboardListener():
 
 @app.route("/")
 def home():
-    thr = Thread(target=keyboardListener, args=[])
-    thr.start() 
+
+        thr = Thread(target=keyboardListener, args=[])
+        thr.start() 
+        return render_template("view.html")
+
+@app.route("/upload_file", methods = ['GET'])
+def upload_file():
+    print("hello")
+    #file = request.files('filename')
+    # file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+    # f = request.files['file']
+    # f.save(file_path)
+    # print(file)
+    # print(os.path.exists(file))
+    selectedFile = view.getElementById('file').files[0]
+    ptt.read_and_interpret_pdf(selectedFile)
     return render_template("view.html")
 
 if __name__ == "__main__":
