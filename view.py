4241917ad3874dab27f133b27e2296fe236d796d
lizzlam <elizabethlam02@gmail.com>
2023-02-11 20:18:02 -0500

    speech to text

--- view.py
+++ view.py
@@ -5,22 +5,31 @@ from pynput import keyboard
 from pynput.keyboard import Key, Listener
 from threading import Thread
 from time import sleep
+import speech_recognition as sr
 
 app = Flask(__name__)
+r = sr.Recognizer()
 
 def on_press(key):
     #print('{0} pressed'.format(
         #key))
     print("Key noticed")
     check_key(key)
-
+    with sr.Microphone() as source:
+        # read the audio data from the default microphone
+        print("Speak now, please.")
+        audio_data = r.record(source, duration=8)
+        print("Recognizing...")
+        # convert speech to text
+        text = r.recognize_google(audio_data)
+        print(text)
 
 def on_release(key):
     #print('{0} release'.format(
        # key))
     # if key == Key.esc:
         # Stop listener
-        print("released")
+    print("released")
 
 
 def check_key(key):
@@ -42,11 +51,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
-
-
-
-
-# Collect events until released
-
-
-
